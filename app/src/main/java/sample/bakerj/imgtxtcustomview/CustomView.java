f55fd4581fad10a4b085c5c9f67af4ea58c9bb92
Administrator <123456>
2016-10-09 13:11:03 +0800

    customview  自定义控件类

--- app/src/main/java/sample/bakerj/imgtxtcustomview/CustomView.java
+++ app/src/main/java/sample/bakerj/imgtxtcustomview/CustomView.java
@@ -1,11 +1,14 @@
 package sample.bakerj.imgtxtcustomview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 
 /**
@@ -14,78 +17,222 @@ import android.view.View;
  */
 
 public class CustomView extends View {
-    /** 图片Bitmap */
+    /**
+     * 图片Bitmap
+     */
     private Bitmap imageBitmap;
-    /** 图片的长宽比 */
+    /**
+     * 图片的长宽比
+     */
     private float imageAspectRatio;
-    /** 图片的透明度 */
+    /**
+     * 图片的透明度
+     */
     private float imageAlpha;
-    /** 图片的左padding*/
+    /**
+     * 图片的左padding
+     */
     private int imagePaddingLeft;
-    /** 图片的上padding */
+    /**
+     * 图片的上padding
+     */
     private int imagePaddingTop;
-    /** 图片的右padding */
+    /**
+     * 图片的右padding
+     */
     private int imagePaddingRight;
-    /** 图片的下padding */
+    /**
+     * 图片的下padding
+     */
     private int imagePaddingBottom;
-    /** 图片伸缩模式 */
+    /**
+     * 图片伸缩模式
+     */
     private int imageScaleType;
-    /** 图片伸缩模式常量 fillXY */
+    /**
+     * 图片伸缩模式常量 fillXY
+     */
     private static final int SCALE_TYPE_FILLXY = 0;
-    /** 图片伸缩模式常量 center */
+    /**
+     * 图片伸缩模式常量 center
+     */
     private static final int SCALE_TYPE_CENTER = 1;
 
-    /** 标题文本内容 */
+    /**
+     * 标题文本内容
+     */
     private String titleText;
-    /** 标题文本字体大小 */
+    /**
+     * 标题文本字体大小
+     */
     private int titleTextSize;
-    /** 标题文本字体颜色 */
+    /**
+     * 标题文本字体颜色
+     */
     private int titleTextColor;
-    /** 标题文本区域左padding */
+    /**
+     * 标题文本区域左padding
+     */
     private int titlePaddingLeft;
-    /** 标题文本区域上padding */
+    /**
+     * 标题文本区域上padding
+     */
     private int titlePaddingTop;
-    /** 标题文本区域右padding */
+    /**
+     * 标题文本区域右padding
+     */
     private int titlePaddingRight;
-    /** 标题文本区域下padding */
+    /**
+     * 标题文本区域下padding
+     */
     private int titlePaddingBottom;
 
-    /** 子标题文本内容 */
+    /**
+     * 子标题文本内容
+     */
     private String subTitleText;
-    /** 子标题文本字体大小 */
+    /**
+     * 子标题文本字体大小
+     */
     private int subTitleTextSize;
-    /** 子标题文本字体颜色 */
+    /**
+     * 子标题文本字体颜色
+     */
     private int subTitleTextColor;
-    /** 子标题文本区域左padding */
+    /**
+     * 子标题文本区域左padding
+     */
     private int subTitlePaddingLeft;
-    /** 子标题文本区域上padding */
+    /**
+     * 子标题文本区域上padding
+     */
     private int subTitlePaddingTop;
-    /** 子标题文本区域右padding */
+    /**
+     * 子标题文本区域右padding
+     */
     private int subTitlePaddingRight;
-    /** 子标题文本区域下padding */
+    /**
+     * 子标题文本区域下padding
+     */
     private int subTitlePaddingBottom;
 
-    /** 控件用的paint */
+    /**
+     * 控件用的paint
+     */
     private Paint paint;
     private TextPaint textPaint;
-    /** 用来界定控件中不同部分的绘制区域 */
+    /**
+     * 用来界定控件中不同部分的绘制区域
+     */
     private Rect rect;
-    /** 宽度和高度的最小值 */
+    /**
+     * 宽度和高度的最小值
+     */
     private static final int MIN_SIZE = 12;
-    /** 控件的宽度 */
+    /**
+     * 控件的宽度
+     */
     private int mViewWidth;
-    /** 控件的高度 */
+    /**
+     * 控件的高度
+     */
     private int mViewHeight;
 
     public CustomView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public CustomView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomView, defStyleAttr, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.CustomView_imageSrc:
+                    imageBitmap = BitmapFactory.decodeResource(
+                            getResources(), a.getResourceId(attr, 0));
+                    break;
+                case R.styleable.CustomView_imageAspectRatio:
+                    imageAspectRatio = a.getFloat(attr, 1.0f);//默认长宽相等
+                    break;
+                case R.styleable.CustomView_imageAlpha:
+                    imageAlpha = a.getFloat(attr, 1.0f);//默认不透明
+                    if (imageAlpha > 1.0f) imageAlpha = 1.0f;
+                    if (imageAlpha < 0.0f) imageAlpha = 0.0f;
+                    break;
+                case R.styleable.CustomView_imagePaddingLeft:
+                    imagePaddingLeft = a.getDimensionPixelSize(attr, 0);
+                    break;
+                case R.styleable.CustomView_imagePaddingTop:
+                    imagePaddingTop = a.getDimensionPixelSize(attr, 0);
+                    break;
+                case R.styleable.CustomView_imagePaddingRight:
+                    imagePaddingRight = a.getDimensionPixelSize(attr, 0);
+                    break;
+                case R.styleable.CustomView_imagePaddingBottom:
+                    imagePaddingBottom = a.getDimensionPixelSize(attr, 0);
+                    break;
+                case R.styleable.CustomView_imageScaleType:
+                    imageScaleType = a.getInt(attr, 0);
+                    break;
+                case R.styleable.CustomView_titleText:
+                    titleText = a.getString(attr);
+                    break;
+                case R.styleable.CustomView_titleTextSize:
+                    titleTextSize = a.getDimensionPixelSize(
+                            attr, (int) TypedValue.applyDimension(
+                                    TypedValue.COMPLEX_UNIT_SP, 25, getResources().getDisplayMetrics()));//默认标题字体大小25sp
+                    break;
+                case R.styleable.CustomView_titleTextColor:
+                    titleTextColor = a.getColor(attr, 0x00000000);//默认黑色字体
+                    break;
+                case R.styleable.CustomView_titleTextPaddingLeft:
+                    titlePaddingLeft = a.getDimensionPixelSize(attr, 0);
+                    break;
+                case R.styleable.CustomView_titleTextPaddingTop:
+                    titlePaddingTop = a.getDimensionPixelSize(attr, 0);
+                    break;
+                case R.styleable.CustomView_titleTextPaddingRight:
+                    titlePaddingRight = a.getDimensionPixelSize(attr, 0);
+                    break;
+                case R.styleable.CustomView_titleTextPaddingBottom:
+                    titlePaddingBottom = a.getDimensionPixelSize(attr, 0);
+                    break;
+                case R.styleable.CustomView_subTitleText:
+                    subTitleText = a.getString(attr);
+                    break;
+                case R.styleable.CustomView_subTitleTextSize:
+                    subTitleTextSize = a.getDimensionPixelSize(attr,
+                            (int) TypedValue.applyDimension(
+                                    20, TypedValue.COMPLEX_UNIT_SP, getResources().getDisplayMetrics()));//默认子标题字体大小20sp
+                    break;
+                case R.styleable.CustomView_subTitleTextColor:
+                    subTitleTextColor = a.getColor(attr, 0x00000000);
+                    break;
+                case R.styleable.CustomView_subTitleTextPaddingLeft:
+                    subTitlePaddingLeft = a.getDimensionPixelSize(attr, 0);
+                    break;
+                case R.styleable.CustomView_subTitleTextPaddingTop:
+                    subTitlePaddingTop = a.getDimensionPixelSize(attr, 0);
+                    break;
+                case R.styleable.CustomView_subTitleTextPaddingRight:
+                    subTitlePaddingRight = a.getDimensionPixelSize(attr, 0);
+                    break;
+                case R.styleable.CustomView_subTitleTextPaddingBottom:
+                    subTitlePaddingBottom = a.getDimensionPixelSize(attr, 0);
+                    break;
+            }
+        }
+        a.recycle();
+
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint = new TextPaint(paint);
+        rect = new Rect();
     }
 }
